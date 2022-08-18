6c221c15eb08eb38af3b4e941c347b6f10509614
Javier Sarmiento <javiersarmiento02@gmail.com>
2022-08-17 19:33:09 -0500

    Update urls.py

--- persona/urls.py
+++ persona/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import *
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('persona/', views.index, name='index'),
     path('documentos/', views.documentos, name='documentos'),
     path('documentos/new/', views.new_document, name='new_document'),
+    path('documentos/edit/<pk>', views.edit_document, name='edit_document'),
+    path('documentos/delete/<pk>', views.delete_document, name='delete_document')
 ]
